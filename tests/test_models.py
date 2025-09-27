009cde738e1794dfed0f1fc0ad41fea274cd7cee
wareefalolayan <wareefalolayan1424@gmail.com>
2025-09-27 21:24:44 +0300

    test save csv

--- tests/test_models.py
+++ tests/test_models.py
@@ -73,13 +73,27 @@ from models import Account, Customer , Bank
 #             self.c1.open_account('xx', initial=10)
 
 class TestBankCSV(unittest.TestCase):
-    def test_load_from_csv_basic(self):
+    # def test_load_from_csv_basic(self):  #only used once to prove load is working
+    #     bank = Bank()            
+    #     x=bank.load_from_csv()
+    #     res=[]
+    #     for c in x:
+    #         bc = c.checking.balance if c.checking else None
+    #         bs = c.savings.balance if c.savings else None
+    #         res.append((c.account_id, c.first_name, c.last_name, c.password,bc, bs, c.active, int(c.overdrafts)))
+    #     expected_customers = [('10001','suresh','sigera','juagw362',1000,10000,True,0),('10002','james', 'taylor','idh36%@#FGd',10000,10000,True,0),('10003','melvin','gordon','uYWE732g4ga1',2000,20000,True,0),('10004','stacey','abrams','DEU8_qw3y72$',2000,20000,True,0),('10005','jake',  'paul',  'd^dg23g)@', 100000,100000, True, 0),('10006','w','w','w',-37,None, False, 2),('10007','w','w','w',-37,None,False, 2)]
+    #     self.assertEqual(res, expected_customers)
+    def test_save_to_csv_basic(self):
         bank = Bank()            
-        x=bank.load_from_csv()
+        bank.load_from_csv() #proven to work
+        bank.add_customer('a','b','c',True, True, 10, 20) #proven to work
+        bank.save_to_csv() #testing this
+        bank2 = Bank()
+        x=bank2.load_from_csv() #reloading to see if saved correctly 
         res=[]
         for c in x:
             bc = c.checking.balance if c.checking else None
             bs = c.savings.balance if c.savings else None
             res.append((c.account_id, c.first_name, c.last_name, c.password,bc, bs, c.active, int(c.overdrafts)))
-        expected_customers = [('10001','suresh','sigera','juagw362',1000,10000,True,0),('10002','james', 'taylor','idh36%@#FGd',10000,10000,True,0),('10003','melvin','gordon','uYWE732g4ga1',2000,20000,True,0),('10004','stacey','abrams','DEU8_qw3y72$',2000,20000,True,0),('10005','jake',  'paul',  'd^dg23g)@', 100000,100000, True, 0),('10006','w','w','w',-37,None, False, 2),('10007','w','w','w',-37,None,False, 2)]
+        expected_customers = [('10001','suresh','sigera','juagw362',1000,10000,True,0),('10002','james', 'taylor','idh36%@#FGd',10000,10000,True,0),('10003','melvin','gordon','uYWE732g4ga1',2000,20000,True,0),('10004','stacey','abrams','DEU8_qw3y72$',2000,20000,True,0),('10005','jake',  'paul',  'd^dg23g)@', 100000,100000, True, 0),('10006','w','w','w',-37,None, False, 2),('10007','w','w','w',-37,None,False, 2),('10008','a','b','c',10,20,True,0)]
         self.assertEqual(res, expected_customers)
